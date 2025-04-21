396e0b0a4e1fbaaf701316c3071c1b11200efe86
Mohammad Slo <mohammad.hamzah.slo@gmail.com>
2025-04-21 23:59:47 +0300

    add german language

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        App::setlocale('ar');
+        App::setlocale('de');
 
         JsonResource::withoutWrapping();
 
