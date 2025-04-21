47b337bc1fbb34a27afdc13a94721d7fc8994e58
Mohammad Slo <mohammad.hamzah.slo@gmail.com>
2025-04-21 13:59:08 +0300

    add languages to the store

---
+++ lang/en/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'home' => 'Home',
+    'hello' => 'Welcome',
+    'about' => 'About Us',
+    'contact' => 'Contact US',
+    'signin' => 'Sign In',
+    'register' => 'Register',
+];
